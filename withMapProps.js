714fdce1a57b7ef10f2054d41ab26ebb84b3904c
Anne <anne.klapwijk@gmail.com>
2018-11-05 21:58:11 +0100

    Simplify by removing `HOC` type

--- withMapProps.js
+++ withMapProps.js
@@ -1,17 +1,14 @@
 // @flow
 
 import { mapProps } from 'recompose';
-import type { HOC } from 'recompose';
 import type { NavigationScreenProp, NavigationStateRoute } from "react-navigation";
 
 type Props = {
   navigation: NavigationScreenProp<NavigationStateRoute>,
 };
 
-const withMapProps: HOC<*, Props> = mapProps((props: Props): Props => {
+export default mapProps((props: Props): Props => {
   return {
     ...props,
   }
 });
-
-export default withMapProps;
